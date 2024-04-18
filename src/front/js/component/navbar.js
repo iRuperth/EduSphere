3ed633145e98f06cefc0c8531c308bdeac8fb6e2
Fergodie <Fero.die@gmail.com>
2024-04-18 14:16:03 +0000

    Se modifica Navbar,Footer, se agrega navbar.css se deja todo responsive

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,23 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Logo from "../../img/logo_acciona.png"
+import "/workspaces/base-evento-acciona/src/front/styles/navbar.css";
 
 export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light bg-light">
-			<div className="d-flex justify-content-between align-items-center w-100 px-3">
-				<Link to="/">
-					<img className="navbar-brand mb-0 " src={Logo} style={{width: '10%'}}/>
-				</Link>
-				<div className="">
-					<Link to="/demo">
-						<button className="btn ">Example button</button>
+			<div className="container-fluid">
+				<div className="d-flex justify-content-between align-items-center px-3" style={{ width: '100%' }}>
+					<Link to="/">
+						<img className="navbar-brand mb-0" src={Logo} alt="Logo" />
 					</Link>
-					<Link to="/single">
-						<button className="btn ">Another button</button>
-					</Link>
+					<div className="d-flex align-items-center">
+						<Link to="/demo" className="me-2">
+							<button className="btn">Example button</button>
+						</Link>
+						<Link to="/single">
+							<button className="btn">Another button</button>
+						</Link>
+					</div>
 				</div>
 			</div>
 		</nav>
+
 	);
 };
