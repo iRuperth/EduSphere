8b53f601a4792c41eabfde87525ac18a415a5911
Roberto Molero Losada <97520685+iRuperth@users.noreply.github.com>
2025-01-30 12:06:29 +0100

    Update navbar.js

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Logo from "../../img/logo_acciona.png"
+import Logo from "../../img/planeta_logo.webp"
 import "/workspaces/base-evento-acciona/src/front/styles/navbar.css";
 
 export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light bg-light">
 			<div className="container-fluid">
-				<div className="d-flex justify-content-between align-items-center px-3" style={{ width: '100%' }}>
+				<div className="d-flex justify-content-between align-items-center px-3 margin-bottom 20px" style={{ width: '100%' }}>
 					<Link to="/">
 						<img className="navbar-brand mb-0" src={Logo} alt="Logo" />
 					</Link>
